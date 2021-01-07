5487b62069e3cc110700d3e5b99f1d023a5c38d9
NoelleKh <kh.noelle@gmail.com>
2021-01-07 21:12:03 +0100

    Contrat Score

--- The Journey/score.py
+++ The Journey/score.py
@@ -2,6 +2,10 @@ from player import Player
 
 
 class Score:
+    """Constructor of the class"""
+    """PRE:/
+       POST: Build the constructor of the class
+    """
     def __init__(self, game):
         self.local_score = 0
         self.best_score = 0
@@ -10,16 +14,23 @@ class Score:
         self.player = Player(self)
 
     def update_score(self):
+        """Udpate the score of the player"""
+        """PRE:/
+           POST: Initilise the attribut point if the game begin or if we lose
+        """
 
-        # quand le jeu commence
         if self.game.start():
             self.points = 0
-        # si le monstre n'a plus de point de vie
+
         elif self.game.player.damage():
             self.best_score = self.points
             self.points = 0
 
     def increase_score(self):
+        """Increase the score"""
+        """PRE:/
+           POST: Incerement the int points inisialized to 0
+        """
         self.points += 1
         if self.points >= 1:
             self.player.attack_up()
